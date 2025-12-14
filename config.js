0fd4fce00ba6e1b5825dc5521b2d24774902b976
Amel20011 <amelcns8@gmail.com>
2025-12-14 21:36:19 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+{
+  "ownerNumber": "+13658700681",
+  "ownerName": "Liviaa Owner",
+  "botName": "Astheric",
+  "footer": "Liviaa Bot 🌸",
+  "prefix": ".",
+  "wm": "LIVIAA ID 🌷",
+  "welcomeEnabledByDefault": true,
+  "antilinkEnabledByDefault": false,
+  "muteEnabledByDefault": false
+}
